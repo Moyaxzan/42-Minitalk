057c86b68001354d8a81bce7e08236b14a92d2db
Tao <tao.spa2003@gmail.com>
2023-08-08 19:57:49 +0200

    communication established but 1 byte is in vacation

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: taospa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 21:39:59 by taospa            #+#    #+#             */
-/*   Updated: 2023/08/07 22:59:37 by taospa           ###   ########.fr       */
+/*   Updated: 2023/08/08 18:38:31 by taospa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,44 @@
 #include <signal.h>
 #include "../libft/libft.h"
 
+int	send_sig(char *str, pid_t pid)
+{
+	int	i;
+	int	ref;
+
+	i = 0;
+	ref = 0b10000000;
+	while (*str)
+	{
+		write(1, str, 1);
+		while (i < 8)
+		{
+			ref = ref >> 1;
+			if (!(*str & ref))
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			i++;
+			usleep(3);
+		}
+		ref = 0b10000000;
+		i = 0;
+		str++;
+	}
+	write(1, "\n", 1);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	pid_t	pid;
 
-	pid = ft_atoi(*argv);
-	kill(pid, SIGUSR1);
+	if (argc != 3)
+		return (write(2, "Invalid argments\n", 17));
+	pid = ft_atoi(argv[1]);
+	if (send_sig(argv[2], pid))
+		write(1, "Signal sent\n", 12);
+	else
+		write(2, "Error sending signal, make sure you used a valid PID\n", 53);
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: taospa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 22:13:50 by taospa            #+#    #+#             */
-/*   Updated: 2023/08/07 22:56:22 by taospa           ###   ########.fr       */
+/*   Updated: 2023/08/08 18:45:46 by taospa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,44 @@
 
 void	signalHandler(int sig)
 {
-	write(1, "Recieving signal...\n", 20);
+	static int	character;
+
+	if (sig == SIGUSR1)
+		character = character << 1;
+	else if (sig == SIGUSR2)
+		character = (character << 1) + 1;
+	else if (!sig)
+	{
+		write(1, &character, 1);
+		character = 0b00000000;
+	}
 }
 
 int	main(void)
 {
-	signal(SIGUSR1, signalHandler);
-	signal(SIGUSR2, signalHandler);
+	struct sigaction	sa;
+	int	i;
 
-	ft_putnbr_fd(getgid(), 1);
-	write(1, "Waiting for signal...\n", 23);
+	i = 0;
+	sa.sa_handler = signalHandler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	sigaction(SIGUSR1, &sa, 0x0);
+	sigaction(SIGUSR2, &sa, 0x0);
+
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
 	while (1)
+	{
+
 		pause();
+		if (i == 7)
+		{
+			i = 0;
+			signalHandler(0);
+		}
+		i++;
+	}
 	return (0);
 }
