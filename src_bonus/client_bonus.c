225a33458b22cb8adf59f35cd7dc5893bdd6b7e5
Tao SAINT PAUL AMOURDAM <tsaint-p@paul-f4Ar8s3.clusters.42paris.fr>
2023-08-30 15:39:08 +0200

    bonus completed ?

--- src_bonus/client_bonus.c
+++ src_bonus/client_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taospa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 21:39:59 by taospa            #+#    #+#             */
-/*   Updated: 2023/08/10 15:42:25 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/08/30 15:33:05 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,20 @@ int	send_nul_byte(pid_t pid)
 	return (0);
 }
 
-int	send_len(char *str, pid_t pid)
+
+int	send_int(pid_t server_pid, int int_to_send)
 {
 	unsigned int	ref;
-	size_t			len;
 	int				success;
 
 	success = 0;
-	len = ft_strlen(str);
 	ref = 0b10000000000000000000000000000000;
 	while (ref)
 	{
-		if (!(len & ref))
-			success = kill(pid, SIGUSR1);
+		if (!(int_to_send & ref))
+			success = kill(server_pid, SIGUSR1);
 		else
-			success = kill(pid, SIGUSR2);
+			success = kill(server_pid, SIGUSR2);
 		ref = ref >> 1;
 		usleep(400);
 	}
@@ -60,7 +59,7 @@ int	send_sig(char *str, pid_t pid)
 
 	success = 0;
 	ref = 0b10000000;
-	if (send_len(str, pid) == -1)
+	if (send_int(pid, getpid()) == -1 || send_int(pid, ft_strlen(str)) == -1)
 		return (-1);
 	while (*str)
 	{
@@ -82,15 +81,26 @@ int	send_sig(char *str, pid_t pid)
 	return (success);
 }
 
+void	signal_handler(int sig)
+{
+	if (sig == SIGUSR1)
+		exit(write(1, "Signal sent successfully\n", 25));
+}
+
 int	main(int argc, char **argv)
 {
-	pid_t	pid;
+	struct sigaction	sa;
+	pid_t				pid;
 
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, 0x0);
 	if (argc != 3)
 		return (write(2, "Invalid argments\n", 17));
 	pid = ft_atoi(argv[1]);
 	if (pid && send_sig(argv[2], pid) != -1)
-		write(1, "Signal sent\n", 12);
+		pause();
 	else
 		write(2, "Error sending signal, make sure you used a valid PID\n", 53);
 	return (0);
--- src_bonus/server_bonus.c
+++ src_bonus/server_bonus.c
@@ -1,16 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taospa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 22:13:50 by taospa            #+#    #+#             */
-/*   Updated: 2023/08/29 12:06:52 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/08/30 15:25:26 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "../libft/libft.h"
 
@@ -25,10 +26,27 @@ void	ft_putstr_ui(unsigned int *str)
 	}
 }
 
+#include <stdio.h>
+int	end_of_transmission(unsigned int *str, pid_t client_pid, int *i)
+{
+	int	success;
+
+	*i = 0;
+	success = 1;
+	ft_putstr_ui(str);
+	write(1, "\n", 1);
+	free(str);
+	str = 0x0;
+	g_in_len = 1;
+	success = kill(client_pid, SIGUSR1);
+	return (success);
+}
+
 void	signal_handler(int sig)
 {
 	static unsigned int	character;
 	static unsigned int	*str;
+	static pid_t		pid_client;
 	static int			i;
 
 	if (sig == SIGUSR1)
@@ -37,20 +55,34 @@ void	signal_handler(int sig)
 		character = (character << 1) + 1;
 	else if (!sig)
 	{
-		if (g_in_len)
+		if (g_in_len == 1)
+			pid_client = character;
+		else if (g_in_len == 2)
 			str = ft_calloc(character + 1, sizeof(unsigned int));
 		else if (!character)
 		{
-			ft_putstr_ui(str);
-			write(1, "\n", 1);
-			free(str);
-			str = 0x0;
-			g_in_len = 1;
-			i = 0;
+			if (end_of_transmission(str, pid_client, &i) == -1)
+				exit(-1);
 		}
 		else if (str)
 			str[i++] = character;
 		character = 0b00000000000000000000000000000000;
+	}
+}
+
+void	check_indicator(int *i)
+{
+	if ((*i == 8 && !g_in_len) || (*i == 32 && g_in_len))
+	{
+		signal_handler(0);
+		if (*i == 32)
+		{
+			if (g_in_len == 1)
+				g_in_len = 2;
+			else
+				g_in_len = 0;
+		}
+		*i = 0;
 	}
 }
 
@@ -70,13 +102,7 @@ int	main(void)
 	while (1)
 	{
 		pause();
-		if ((i == 8 && !g_in_len) || (i == 32 && g_in_len))
-		{
-			signal_handler(0);
-			if (i == 32)
-				g_in_len = 0;
-			i = 0;
-		}
+		check_indicator(&i);
 		i++;
 	}
 	return (0);
